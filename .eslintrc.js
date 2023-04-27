eae2f44fd5c8c0c8dd94a27a006a08f8722f5454
Kishan Nath <kishansnath93@gmail.com>
2023-04-27 21:45:17 +0530

    eslist fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     ],
     rules: {
         // override/add rules settings here, such as:
-        // 'vue/no-unused-vars': 'error'
+        'vue/no-unused-vars': 'error',
         // no semicolons
         'semi': ['error', 'never'],
     }
